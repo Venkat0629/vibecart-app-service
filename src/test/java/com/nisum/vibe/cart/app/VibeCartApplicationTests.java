aca2817a9ad84fbe9a05a8a380f1295d98ed1657
Venkat0629 <venkatsaimane@gmail.com>
2024-10-11 11:32:35 +0530

    Changed names for test files

---
+++ src/test/java/com/nisum/vibe/cart/app/VibeCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.vibe.cart.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
